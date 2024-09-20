65f5191cd42db44514ac13c114337e3b4602ac8b
BigPig <1186725127@QQ.com>
2024-09-20 15:46:11 +0800

    240920 Create dct8x8.sv 构建接口，待设计DCT

---
+++ src/Encoder_Jpeg/dct8x8.sv
@@ -0,0 +1,23 @@
+module dct8x8 (
+  input                           i_clk,
+  input                           i_rst_n,
+
+  input    [63:0][7:0]            i_img_data, 
+  input                           i_img_data_vaild,
+
+  output signed reg [63:0][15:0]  o_dct_data,
+  output reg                      o_dct_data_vaild
+);
+  
+/******************************     将输入0-255偏置为-128~127     **********************************/
+  logic signed [63:0][7:0]       img_data_d;
+  integer i;
+  always@(posedge i_clk)begin
+    for(i=0;i<=63;i=i+1)begin
+      img_data_d <= img_data - 128;
+    end
+  end
+  
+  /******************************     DCT     **********************************/
+
+endmodule

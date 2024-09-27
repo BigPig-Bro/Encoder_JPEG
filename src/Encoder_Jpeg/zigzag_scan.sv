307cd25fa4550caf21211a12802aa58d3f89f1cc
BigPig <1186725127@QQ.com>
2024-09-27 18:21:06 +0800

    240927 完成部分转换设计

---
+++ src/Encoder_Jpeg/zigzag_scan.sv
@@ -0,0 +1,88 @@
+module zigzag_scan(
+  input     i_clk ,
+  input     i_rst_n,
+
+  input [63:0][15:0]  i_quant_data,
+  input               i_quant_data_vaild,
+
+  output reg [63:0][15:0]  o_zig_data,
+  output reg               o_zig_vaild,
+);
+
+  integer i;
+  always@(posedge i_clk)
+    if(!i_rst_n)begin
+      o_zig_vaild <= 'd0;
+      for(i=0;i<64;i++)begin
+        o_zig_data[i] <= 'd0;
+      end
+    end else begin
+      o_zig_vaild <= i_quant_data_vaild;
+
+      o_zig_data[0] <= i_quant_data[0];
+      o_zig_data[1] <= i_quant_data[1];
+      o_zig_data[2] <= i_quant_data[8];
+      o_zig_data[3] <= i_quant_data[16];
+      o_zig_data[4] <= i_quant_data[9];
+      o_zig_data[5] <= i_quant_data[0];
+      o_zig_data[6] <= i_quant_data[0];
+      o_zig_data[7] <= i_quant_data[0];
+      o_zig_data[8] <= i_quant_data[0];
+      o_zig_data[9] <= i_quant_data[0];
+      o_zig_data[10] <= i_quant_data[0];
+      o_zig_data[11] <= i_quant_data[0];
+      o_zig_data[12] <= i_quant_data[0];
+      o_zig_data[13] <= i_quant_data[0];
+      o_zig_data[14] <= i_quant_data[0];
+      o_zig_data[15] <= i_quant_data[0];
+      o_zig_data[16] <= i_quant_data[0];
+      o_zig_data[17] <= i_quant_data[0];
+      o_zig_data[18] <= i_quant_data[0];
+      o_zig_data[19] <= i_quant_data[0];
+      o_zig_data[20] <= i_quant_data[0];
+      o_zig_data[21] <= i_quant_data[0];
+      o_zig_data[22] <= i_quant_data[0];
+      o_zig_data[23] <= i_quant_data[0];
+      o_zig_data[24] <= i_quant_data[0];
+      o_zig_data[25] <= i_quant_data[0];
+      o_zig_data[26] <= i_quant_data[0];
+      o_zig_data[27] <= i_quant_data[0];
+      o_zig_data[28] <= i_quant_data[0];
+      o_zig_data[29] <= i_quant_data[0];
+      o_zig_data[30] <= i_quant_data[0];
+      o_zig_data[31] <= i_quant_data[0];
+      o_zig_data[32] <= i_quant_data[0];
+      o_zig_data[33] <= i_quant_data[0];
+      o_zig_data[34] <= i_quant_data[0];
+      o_zig_data[35] <= i_quant_data[0];
+      o_zig_data[36] <= i_quant_data[0];
+      o_zig_data[37] <= i_quant_data[0];
+      o_zig_data[38] <= i_quant_data[0];
+      o_zig_data[39] <= i_quant_data[0];
+      o_zig_data[40] <= i_quant_data[0];
+      o_zig_data[41] <= i_quant_data[0];
+      o_zig_data[42] <= i_quant_data[0];
+      o_zig_data[43] <= i_quant_data[0];
+      o_zig_data[44] <= i_quant_data[0];
+      o_zig_data[45] <= i_quant_data[0];
+      o_zig_data[46] <= i_quant_data[0];
+      o_zig_data[47] <= i_quant_data[0];
+      o_zig_data[48] <= i_quant_data[0];
+      o_zig_data[49] <= i_quant_data[0];
+      o_zig_data[50] <= i_quant_data[0];
+      o_zig_data[51] <= i_quant_data[0];
+      o_zig_data[52] <= i_quant_data[0];
+      o_zig_data[53] <= i_quant_data[0];
+      o_zig_data[54] <= i_quant_data[0];
+      o_zig_data[55] <= i_quant_data[0];
+      o_zig_data[56] <= i_quant_data[0];
+      o_zig_data[57] <= i_quant_data[0];
+      o_zig_data[58] <= i_quant_data[0];
+      o_zig_data[59] <= i_quant_data[0];
+      o_zig_data[60] <= i_quant_data[0];
+      o_zig_data[61] <= i_quant_data[0];
+      o_zig_data[62] <= i_quant_data[0];
+      o_zig_data[63] <= i_quant_data[0];
+    end
+
+endmodule
